56cc8f882f9fd694cc8fd8cc76900b3e8aac98d7
Olivia McCallum <o.mccallum0220211@arts.ac.uk>
2022-01-31 17:24:54 +0000

    removed selection logic, moved key controls to update and move methods (separate for each player)

--- players.py
+++ players.py
@@ -1,9 +1,27 @@
 import pygame
 from states import States
-from selection_menu_1 import SelectionMenu1
-from selection_menu_2 import SelectionMenu2
+import selection_menu_1
+import selection_menu_2
 import world
 import game
+
+# old player1 actions
+moving_down1 = False
+moving_up1 = False
+moving_right1 = False
+moving_left1 = False
+shoot1 = False
+missile1 = False
+missile_shot1 = False
+
+# old player2 actions
+moving_down2 = False
+moving_up2 = False
+moving_right2 = False
+moving_left2 = False
+shoot2 = False
+missile2 = False
+missile_shot2 = False
 
 class Spaceship1(pygame.sprite.Sprite):
     def __init__(self, x, y, scale, speed, health, missiles, character):
@@ -12,11 +30,6 @@ class Spaceship1(pygame.sprite.Sprite):
 
         self.character = character
 
-        selection1 = SelectionMenu1()
-        for key in selection1.options_dict.keys():
-            if key == 'selected':
-                self.character = key
-
         self.alive = True
 
         self.speed = speed
@@ -24,6 +37,7 @@ class Spaceship1(pygame.sprite.Sprite):
         self.shoot_cooldown = 0
         self.damage_cooldown = 0
         self.missiles = missiles
+        self.score = 0
 
         # for health bar
         self.health = health
@@ -35,9 +49,14 @@ class Spaceship1(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
 
-    def update(self):
-        # TODO: self.update_cooldowns for special powers
+    def update(self, keys):
         self.check_alive()
+
+        # key controls
+        if keys[pygame.K_RCTRL]:
+            self.shoot()
+        if keys [pygame.K_RSHIFT]:
+            Missile(self.rect.centerx + 20, self.rect.centery, 1)
 
         if self.shoot_cooldown > 0:
             self.shoot_cooldown -= 1
@@ -45,32 +64,28 @@ class Spaceship1(pygame.sprite.Sprite):
         if self.damage_cooldown > 0:
             self.damage_cooldown -= 1
 
+        # collision damage
         for player in world.player_group:
             if pygame.sprite.spritecollide(player, world.enemy_group_upper, False):
                 if player.alive:
                     if self.damage_cooldown == 0:
                         self.damage_cooldown = 60
                         player.health -= 1
-            if pygame.sprite.spritecollide(player, world.enemy_group_lower, False):
-                if player.alive:
-                    if self.damage_cooldown == 0:
-                        self.damage_cooldown = 60
-                        player.health -= 1
 
-    def move(self, moving_down, moving_up, moving_right, moving_left):
+    def move(self, keys):
         # reset movement variables
         world.screen_scroll = 0
         dy = 0
         dx = 0
 
         # assign movement values
-        if moving_down:
+        if keys[pygame.K_DOWN]:
             dy = self.speed
-        if moving_up:
+        if keys[pygame.K_UP]:
             dy = -self.speed
-        if moving_right:
+        if keys[pygame.K_RIGHT]:
             dx = self.speed
-        if moving_left:
+        if keys[pygame.K_LEFT]:
             dx = -self.speed
 
         # update rectangle position
@@ -101,13 +116,19 @@ class Spaceship1(pygame.sprite.Sprite):
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
-        #pygame.draw.rect(screen, RED, self.rect, 1)
 
     def shoot(self):
         if self.shoot_cooldown == 0:
             self.shoot_cooldown = 20
             laser = PlayerLaser(self.rect.right + 3, self.rect.centery)
             world.laser_group_player.add(laser)
+
+    def total_score(self):
+        # counts dead enemies within player 1's zone
+        for enemy in world.enemy_group_upper:
+            if not enemy.alive:
+                self.score += 1
+        return self.score
 
 class Spaceship2(Spaceship1):
     def __init__(self, x, y, scale, speed, health, missiles, character):
@@ -116,11 +137,6 @@ class Spaceship2(Spaceship1):
 
         self.character = character
 
-        selection2 = SelectionMenu2()
-        for key in selection2.options_dict.keys():
-            if key == 'selected':
-                self.character = key
-
         self.alive = True
 
         self.speed = speed
@@ -128,6 +144,7 @@ class Spaceship2(Spaceship1):
         self.shoot_cooldown = 0
         self.damage_cooldown = 0
         self.missiles = missiles
+        self.score = 0
 
         # for health bar
         self.health = health
@@ -139,20 +156,41 @@ class Spaceship2(Spaceship1):
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
 
-    def move(self, moving_down, moving_up, moving_right, moving_left):
+    def update(self, keys):
+        self.check_alive()
+
+        if self.shoot_cooldown > 0:
+            self.shoot_cooldown -= 1
+
+        if self.damage_cooldown > 0:
+            self.damage_cooldown -= 1
+
+        for player in world.player_group:
+            if pygame.sprite.spritecollide(player, world.enemy_group_lower, False):
+                if player.alive:
+                    if self.damage_cooldown == 0:
+                        self.damage_cooldown = 60
+                        player.health -= 1
+
+        if keys[pygame.K_CAPSLOCK]:
+            self.shoot()
+        if keys[pygame.K_x]:
+            Missile(self.rect.centerx + 20, self.rect.centery, 1)
+
+    def move(self, keys):
         # reset movement variables
         world.screen_scroll = 0
         dy = 0
         dx = 0
 
         # assign movement values
-        if moving_down:
+        if keys[pygame.K_s]:
             dy = self.speed
-        if moving_up:
+        if keys[pygame.K_w]:
             dy = -self.speed
-        if moving_right:
+        if keys[pygame.K_d]:
             dx = self.speed
-        if moving_left:
+        if keys[pygame.K_a]:
             dx = -self.speed
 
         # update rectangle position
@@ -173,6 +211,12 @@ class Spaceship2(Spaceship1):
 
         return world.screen_scroll
 
+    def score(self, kills):
+        for enemy in world.enemy_group_lower:
+            if not enemy.alive:
+                self.score += kills
+        return self.score
+
 class PlayerLaser(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
@@ -182,13 +226,14 @@ class PlayerLaser(pygame.sprite.Sprite):
         self.rect.center = (x, y)
 
     def update(self):
-        #pygame.draw.rect(screen, RED, self.rect, 1)
+        # scroll
         self.rect.x += self.speed + world.screen_scroll
 
         # delete if off screen
-        if self.rect.right > SCREEN_WIDTH:
+        if self.rect.right > world.SCREEN_WIDTH:
             self.kill()
 
+        # lasers affect enemies in all zones
         for enemy in world.enemy_group_upper:
             if pygame.sprite.spritecollide(enemy, world.laser_group_player, False):
                 if enemy.alive:
@@ -213,7 +258,7 @@ class Missile(pygame.sprite.Sprite):
         self.rect.x += self.speed + world.screen_scroll
 
         # delete if off screen
-        if self.rect.right > SCREEN_WIDTH:
+        if self.rect.right > world.SCREEN_WIDTH:
             self.kill()
 
         # do damage
