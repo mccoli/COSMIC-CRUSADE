dda766df5cfcd646aa42a69d27a12605e914643a
Olivia McCallum <o.mccallum0220211@arts.ac.uk>
2022-01-31 17:22:36 +0000

    created a second dictionary to overwrite the first (doesn't work)

--- selection_menu_2.py
+++ selection_menu_2.py
@@ -13,12 +13,19 @@ class SelectionMenu2(States):
         rogue_select_img = pygame.image.load('img/icons/rogue_select.png').convert_alpha()
         warrior_select_img = pygame.image.load('img/icons/oriel_select.png').convert_alpha()
         mage_select_img = pygame.image.load('img/icons/zinta_select.png').convert_alpha()
-        # create buttons
+        # create dictionary to hold buttons
         self.options_dict = {
             'healer_select': Button(5, 240, healer_select_img, 1, command=self.switch_state),
             'rogue_select': Button(205, 240, rogue_select_img, 1, command=self.switch_state),
             'warrior_select': Button(405, 240, warrior_select_img, 1, command=self.switch_state),
             'mage_select': Button(605, 240, mage_select_img, 1, command=self.switch_state)
+        }
+        # dictionary with altered keys
+        self.selected_dict = {
+            'healer_selected': Button(5, 240, healer_select_img, 1, command=self.switch_state),
+            'rogue_selected': Button(205, 240, rogue_select_img, 1, command=self.switch_state),
+            'warrior_selected': Button(405, 240, warrior_select_img, 1, command=self.switch_state),
+            'mage_selected': Button(605, 240, mage_select_img, 1, command=self.switch_state)
         }
 
     def cleanup(self):
@@ -32,13 +39,18 @@ class SelectionMenu2(States):
         player2_select_img = pygame.image.load('img/background/player2_select.png').convert_alpha()
         screen.blit(player2_select_img, (0, 0))
 
-        for value in self.options_dict.values():
+        # draw buttons to screen
+        for key, value in self.options_dict.items():
             value.draw(screen)
+            
+    def select(self, key):
+        # change key to 'selected' if event is triggered
+        self.options_dict['selected'] = self.options_dict.pop(key)
 
     def get_event(self, event):
+        # trigger event if button is clicked
         for key, value in self.options_dict.items():
             value.get_event(event)
-            key = 'selected'
 
-    def update(self, screen, dt):
+    def update(self, screen, dt, keys):
         self.draw(screen)
