07d5ea2ef74429711e596eecfef8d23bb2657c5c
Olivia McCallum <o.mccallum0220211@arts.ac.uk>
2022-01-31 17:29:36 +0000

    added keys to update method

--- control.py
+++ control.py
@@ -1,5 +1,9 @@
 import pygame
 from states import States
+from start_menu import StartMenu
+from game import Game
+from selection_menu_1 import SelectionMenu1
+from selection_menu_2 import SelectionMenu2
 
 class Control:
     def __init__(self, **settings):
@@ -7,13 +11,12 @@ class Control:
         self.done = False
         self.screen = pygame.display.set_mode(self.size)
         self.clock = pygame.time.Clock()
-        self.keys = pygame.key.get_pressed()
-
+    # initial set up of all states
     def setup_states(self, state_dict, start_state):
         self.state_dict = state_dict
         self.state_name = start_state
         self.state = self.state_dict[self.state_name]
-
+    # moves current state to the next
     def flip_state(self):
         self.state.done = False
         previous, self.state_name = self.state_name, self.state.next
@@ -23,18 +26,19 @@ class Control:
         self.state.previous = previous
 
     def update(self, dt):
+        keys = pygame.key.get_pressed()
         if self.state.quit:
             self.done = True
         elif self.state.done:
             self.flip_state()
-        self.state.update(self.screen, dt)
-
+        self.state.update(self.screen, dt, keys)
+    # only event loop in the program because it collects events from all other states
     def event_loop(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.done = True
             self.state.get_event(event)
-
+    
     def main_game_loop(self):
         while not self.done:
             delta_time = self.clock.tick(self.fps)/1000.0
